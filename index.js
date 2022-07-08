2b102e464e3f0019a994abfc619a3b34273f6f05
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-08 09:42:43 -0700

    refactor method to keep track of state

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+// use an object to keep track of state
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+  };
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
@@ -23,9 +22,8 @@ function logTaskState(taskIndex) {
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(tasks);
