b0bf4635af4a87f51d41ed1c58e3b41acf3ba3fa
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-08 09:48:32 -0700

    add methods to object instead of global functions

--- index.js
+++ index.js
@@ -4,27 +4,22 @@ function newTask(title, description) {
     title: title,
     description: description,
     complete: false,
+    logState: function() {
+      console.log(`${this.title} has ${this.complete ? " " : "not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-console.log(tasks);
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
